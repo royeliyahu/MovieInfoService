44a18ded26538877c2a1829e6dd4255c86f7aef3
RoyE <royeliyahu827@gmail.com>
2019-07-07 17:42:43 +0300

    adding eureka-client (discovery service)

--- src/main/java/io/javabrains/MovieInfoService/MovieInfoServiceApplication.java
+++ src/main/java/io/javabrains/MovieInfoService/MovieInfoServiceApplication.java
@@ -2,8 +2,10 @@ package io.javabrains.MovieInfoService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+//@EnableEurekaClient
 public class MovieInfoServiceApplication {
 
 	public static void main(String[] args) {
